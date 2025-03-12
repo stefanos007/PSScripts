4e04453f3ef57d9ede1a20029a302d596611d55d
Stefanos Daniil <omgandthreelols@gmail.com>
2025-03-12 02:46:56 +0200

    .

---
+++ Get-SecurityEvents.ps1
@@ -0,0 +1,72 @@
+[CmdletBinding()]param(
+    [Parameter(ParameterSetName = "TargetOnly", Mandatory = $true)]
+    [Parameter(ParameterSetName = "Both", Mandatory = $true)]
+    [string[]]$Target,
+    [Parameter(ParameterSetName = "SubjectOnly", Mandatory = $true)]
+    [Parameter(ParameterSetName = "Both", Mandatory = $true)]
+    [string[]]$Subject,
+    [Parameter(ParameterSetName = "Both", Mandatory = $true)]
+    [Parameter(DontShow)]
+	[ValidateSet("or","and")]
+	[string]$Operation,
+	[string]$Id,
+	[int]$MaxEvents=25
+)
+
+if(!([string]::IsNullOrEmpty($Id)))
+{
+	if ($PSCmdlet.ParameterSetName -eq "Both") 
+	{
+		$query = "*[System[EventID=$Id] and (("
+		$Target | foreach { $query += "EventData[Data[@Name='TargetUserName']=`'$_`'] or " }
+		$query = $query.TrimEnd(" or ") + ") $Operation ("
+		$Subject | foreach { $query += "EventData[Data[@Name='SubjectUserName']=`'$_`'] or " }
+		$query = $query.TrimEnd(" or ") + "))]"
+	}
+	if ($PSCmdlet.ParameterSetName -eq "TargetOnly") 
+	{
+		$query = "*[System[EventID=$Id] and (("
+		$Target | foreach { $query += "EventData[Data[@Name='TargetUserName']=`'$_`'] or " }
+		$query = $query.TrimEnd(" or ") + "))]"
+	}
+	if ($PSCmdlet.ParameterSetName -eq "SubjectOnly") 
+	{
+		$query = "*[System[EventID=$Id] and ((" 
+		$Subject | foreach { $query += "EventData[Data[@Name='SubjectUserName']=`'$_`'] or " }
+		$query = $query.TrimEnd(" or ") + "))]"
+	}
+}
+else
+{
+	if ($PSCmdlet.ParameterSetName -eq "Both") 
+	{
+		$query = "*[("
+		$Target | foreach { $query += "EventData[Data[@Name='TargetUserName']=`'$_`'] or " }
+		$query = $query.TrimEnd(" or ") + ") $Operation ("
+		$Subject | foreach { $query += "EventData[Data[@Name='SubjectUserName']=`'$_`'] or " }
+		$query = $query.TrimEnd(" or ") + ")]"
+	}
+	if ($PSCmdlet.ParameterSetName -eq "TargetOnly") 
+	{
+		$query = "*[("
+		$Target | foreach { $query += "EventData[Data[@Name='TargetUserName']=`'$_`'] or " }
+		$query = $query.TrimEnd(" or ") + ")]"
+	}
+	if ($PSCmdlet.ParameterSetName -eq "SubjectOnly") 
+	{
+		$query = "*[(" 
+		$Subject | foreach { $query += "EventData[Data[@Name='SubjectUserName']=`'$_`'] or " }
+		$query = $query.TrimEnd(" or ") + ")]"
+	}
+}
+
+$htmlBuilder = "<html><head><style>table, th, td {  border: 1px solid;border-collapse: collapse; }th, td { padding: 15px; } tr:nth-child(even) {background-color: #f2f2f2;} tr:hover {background-color: coral;} .tables { margin: auto; }</style></head><body>"
+$htmlBuilder += "<div class='tables'><h2>[$($(hostname).ToUpper())]</h2><table><tr><th>Property</th><th>Value</th></tr>"
+
+foreach($log in (Get-WinEvent -FilterXPath $query -LogName Security -MaxEvents $MaxEvents))
+{
+	$htmlBuilder += "<tr><th colspan='2'>ID: $($log.Id) <br> Created On: $($log.TimeCreated) <br> Level: $($log.LevelDisplayName)</th></tr>"
+	$([xml]$log.ToXml()).Event.EventData.Data | foreach{ $htmlBuilder += "<tr><td><b>$($_.Name)</b></td><td>$($_.'#text')</td></tr>" }
+}
+$htmlBuilder += "</table></body></html>"
+Out-File -FilePath "C:\Users\$([Environment]::UserName)\Desktop\sec_events.html" -InputObject $htmlBuilder -Force
